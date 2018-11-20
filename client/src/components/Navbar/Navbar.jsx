89705f215eef744970f10ca17cc4845e2e93819e
Adam Espinoza <adamespi@usc.edu>
2018-11-20 10:44:28 -0800

    Merge

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+
+	render() {
+		return(
+			<nav className="navbar navbar-light bg-light">
+				<h1>🍳 Consensus Cookbook </h1>
+			</nav>
+		);
+	}
+}
+ 
+export default Navbar;
